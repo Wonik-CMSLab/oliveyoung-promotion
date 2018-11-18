e06acff56114797dd3570a9ff6879e2f65deb3ac
ShinHyunJong <kashiashin@gmail.com>
2018-11-18 18:46:49 +0900

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba652dba5938c443ca8a70aa4e1ff47c.js"
+  "/oliveyoung-promotion/precache-manifest.e1fd7103da4baefa6e5f6b4d8d014d6c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/oliveyoung-promotion/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
