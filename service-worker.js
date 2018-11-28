718973e4f7aee97a24dc47f7b75baac6ed5e1858
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2018-11-28 23:08:40 +0900

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/oliveyoung-promotion/precache-manifest.fefd57a05cc9b0a85b140f16f6ebb999.js"
+  "/oliveyoung-promotion/precache-manifest.18cadb6dab4019c9746a4fc5b57d1b21.js"
 );
 
 workbox.clientsClaim();
